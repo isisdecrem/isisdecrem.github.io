7828c39cbd91269a91d99f2dfb95d38cb2bc5862
Isis Decrem <60768505+isisdecrem@users.noreply.github.com>
2022-08-03 15:02:09 -0700

    other signup info

---
+++ User.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(username, email, fName, lName, school, userID){
+        this.username= username;
+        this.email = email; 
+        this.fName = fName;
+        this.lName = lName; 
+        this.school = school; 
+        this.userID = userID; 
+    }
+}
