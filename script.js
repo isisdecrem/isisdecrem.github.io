df0b4b7f8259c67fc1c5395ba94470d9e3fe2a33
Isis Decrem <60768505+isisdecrem@users.noreply.github.com>
2022-08-06 10:55:40 -0700

    cont database

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+//
+const database = getDatabase(app);
  
+
 //login button = submitButton
 const login = document.getElementById("signin");
 const logInButton= document.getElementById("signin-btn");
